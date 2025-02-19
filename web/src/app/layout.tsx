712b17174ac95edf1fb708e5834eaf6f8ce0d15f
rita lanca <rita.lanca17@gmail.com>
2025-02-19 08:22:26 +0000

    Styles home page

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,31 +1,33 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Montserrat, Oxanium} from 'next/font/google';
+import './globals.css'
+import type { Metadata } from 'next'
+import { Montserrat, Oxanium } from 'next/font/google'
 
 export const metadata: Metadata = {
-  title: "devstage",
-};
+  title: 'devstage',
+}
 
 const oxanium = Oxanium({
-  weight: ["500", "600"],
-  subsets: ["latin"], // nao carrega todos os simbolos do mundo
-  variable: "--font-oxanium", // definir a variavel para a fonte
+  weight: ['500', '600'],
+  subsets: ['latin'], // nao carrega todos os simbolos do mundo
+  variable: '--font-oxanium', // definir a variavel para a fonte
 })
 
 const montserrat = Montserrat({
-  weight: ["400", "600"],
-  subsets: ["latin"], // nao carrega todos os simbolos do mundo
-  variable: "--font-montserrat", // definir a variavel para a fonte
+  weight: ['400', '600'],
+  subsets: ['latin'], // nao carrega todos os simbolos do mundo
+  variable: '--font-montserrat', // definir a variavel para a fonte
 })
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     // Para deixar as fontes disponiveis na pagina precisamos de passar no className
     <html lang="en" className={`${oxanium.variable} ${montserrat.variable}`}>
-      <body className="bg-gray-900 text-gray-100 antialiased">{children}</body>
+      <body className="bg-gray-900 text-gray-100 antialiased bg-[url(/background.png)] bg-no-repeat bg-top md:bg-right-top">
+        {children}
+      </body>
     </html>
-  );
+  )
 }
