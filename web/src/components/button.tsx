16ebc999a8f73a3a68b44875ac098b63fba4af6d
rita lanca <rita.lanca17@gmail.com>
2025-02-19 07:29:46 +0000

    Creates and style button and input components

---
+++ web/src/components/button.tsx
@@ -0,0 +1,18 @@
+import React, { type ComponentProps } from 'react'
+
+interface ButtonProps extends ComponentProps<'button'> {}
+
+const Button = (props: ButtonProps) => {
+  return (
+    <button
+      className="px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl 
+      w-full cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300
+      flex justify-between items-center"
+      {...props}
+    >
+      {props.children}
+    </button>
+  )
+}
+
+export default Button
