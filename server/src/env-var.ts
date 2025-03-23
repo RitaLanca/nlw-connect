770ce8602167816d0e5351ae766b1240917a5066
rita lanca <rita.lanca17@gmail.com>
2025-03-23 17:27:12 +0000

    chore(env variables): add new variable

--- server/src/env-var.ts
+++ server/src/env-var.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  // NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  NODE_ENV: z.enum(['dev', 'staging', 'production']).default('dev'),
   // JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3333), // try to convert to a number, if fails, default to 3333
   WEB_URL: z.string().url(),
