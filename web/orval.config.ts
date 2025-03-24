037883fd855a70c7f4618241acf3f2cd402b53cf
rita lanca <rita.lanca17@gmail.com>
2025-03-24 19:55:51 +0000

    chore(orval): fix base url to use env variables

--- web/orval.config.ts
+++ web/orval.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'orval'
 
 export default defineConfig({
   api: {
-    input: 'http://localhost:3333/docs/json',
+    input: process.env.NODE_ENV === 'production'? `${process.env.API_URL}/docs/json`  : 'http://localhost:3333/docs/json',
     output: {
       target: './src/http/api.ts',
       client: 'fetch',
       httpClient: 'fetch',
       clean: true, // clean up the output directory before generation
-      baseUrl: 'http://localhost:3333', // base api url
+      baseUrl: process.env.NODE_ENV === 'production'? process.env.API_URL : 'http://localhost:3333', // base api url
       override: {
         fetch: {
           includeHttpResponseReturnType: false, // don't include the response status code
